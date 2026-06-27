c1af7149051b60901023a77317404d6e771ddb0d
vanillacrepes <daveplayzminecraft@gmail.com>
2026-06-27 21:02:54 +0800

    added tray icon

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,25 +1,45 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use tauri_plugin_deep_link::DeepLinkExt;
-use tauri::{Emitter, Manager};
+use tauri::{ Emitter, Manager };
+use tauri::menu::{ Menu, MenuItem };
+use tauri::tray::TrayIconBuilder;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder
         ::default()
-        .plugin(tauri_plugin_single_instance::init(|app, argv, _cwd| {
-            if let Some(url) = argv.get(1) {
-                let _ = app.emit("deep-link-urls", vec![url.clone()]);
-            }
-            if let Some(window) = app.get_webview_window("main") {
-                let _ = window.set_focus();
-            }
-        }))
+        .plugin(
+            tauri_plugin_single_instance::init(|app, argv, _cwd| {
+                if let Some(url) = argv.get(1) {
+                    let _ = app.emit("deep-link-urls", vec![url.clone()]);
+                }
+                if let Some(window) = app.get_webview_window("main") {
+                    let _ = window.set_focus();
+                }
+            })
+        )
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_deep_link::init())
 
         .setup(|app| {
             app.deep_link().register_all()?;
+
+            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+
+            let menu = Menu::with_items(app, &[&quit_i])?;
+            let _tray = TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .menu(&menu)
+                .on_menu_event(|app, event| {
+                    match event.id.as_ref() {
+                        "quit" => {
+                            app.exit(0);
+                        }
+                        _ => {}
+                    }
+                })
+                .build(app)?;
 
             #[cfg(debug_assertions)]
             {
